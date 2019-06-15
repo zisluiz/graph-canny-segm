4a2e2fcf53d1b0274fde1df477052527c883c855
Zis <zisluiz@gmail.com>
2019-06-15 19:53:30 -0300

    Changing to support boost python integration with obeach framework

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ *.cpp -shared -fPIC `pkg-config --cflags opencv --libs opencv` -lm -std=c++11 -lboost_system -o libgraph-canny-segm.so
+	g++ *.cpp -shared -fPIC -I/usr/include/python3.6 -I/usr/lib/x86_64-linux-gnu/ `pkg-config --cflags opencv --libs opencv` -lm -std=c++11 -lboost_system -lboost_python3 -lpython3.6m -o libgraph_canny_segm.so
 
 debug:
-	g++ *.cpp `pkg-config --cflags opencv --libs opencv` -lm -std=c++11 -lboost_system -o graph-canny-segm.d.so	
+	g++ *.cpp -I/usr/include/python3.6 -I/usr/lib/x86_64-linux-gnu/ `pkg-config --cflags opencv --libs opencv` -lm -std=c++11 -lboost_system -lboost_python3 -lpython3.6m -o graph_canny_segm.d.so	
--- facade.cpp
+++ facade.cpp
@@ -1,3 +1,7 @@
+#define PY_ARRAY_UNIQUE_SYMBOL pbcvt_ARRAY_API
+
+#include <boost/python.hpp>
+#include "pyboostcvconverter.hpp"
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -5,50 +9,55 @@
 #include "configprop.h"
 #include "objectseg.h"
 
+namespace pbcvt {
+
+    using namespace boost::python;
+
 class Facade
 {
     public:
         Facade(std::string configFilePath);
-        ObjectSeg* segmentImage(const char* rgbFilePath, const char* depthFilePath, char *numObjects);
-        void cleanupObjects(ObjectSeg* objectToClean, char *numObjects);
+        boost::python::list segmentImage(cv::Mat rgbImage, cv::Mat depthImage);
+        void cleanupObjects(boost::python::list objectToClean);
+        int numObjects;
     private:
-    bool showDebug;
-    bool showImages;
-    int k=38; //50;
-    int kx=2000;
-    int ky=30;
-    int ks=50;
-    float kdv=4.5f;
-    float kdc=0.1f;
-    float min_size=500.0f;
-    float sigma=0.8f;
-    float max_ecc = 0.978f;
-    float max_L1 = 3800.0f;
-    float max_L2 = 950.0f;
-    int DTH = 30; //[mm]
-    int plusD = 30; //7; //for depth boundary
-    int point3D = 5; //10//for contact boundary
-    int g_angle = 154; //140;//148;//2.f/3.f*M_PI;
-    int l_angle = 56; //M_PI/3.f;
-    int Lcanny = 50;
-    int Hcanny = 75;
-    int FarObjZ = 28000; //875;//1800; //[mm]   
+        bool showDebug;
+        bool showImages;
+        int k=38; //50;
+        int kx=2000;
+        int ky=30;
+        int ks=50;
+        float kdv=4.5f;
+        float kdc=0.1f;
+        float min_size=500.0f;
+        float sigma=0.8f;
+        float max_ecc = 0.978f;
+        float max_L1 = 3800.0f;
+        float max_L2 = 950.0f;
+        int DTH = 30; //[mm]
+        int plusD = 30; //7; //for depth boundary
+        int point3D = 5; //10//for contact boundary
+        int g_angle = 154; //140;//148;//2.f/3.f*M_PI;
+        int l_angle = 56; //M_PI/3.f;
+        int Lcanny = 50;
+        int Hcanny = 75;
+        int FarObjZ = 28000; //875;//1800; //[mm]   
 
-    //ACCV
-    double fx = 572.41140;
-    double fy = 573.57043;
-    double cx = 325.26110;
-    double cy = 242.04899;    
-    
-    float k_vec[9];
-    float kfloat;
-    float kxfloat;
-    float kyfloat;
-    float ksfloat;
-    float gafloat;
-    float lafloat;
-    float lcannyf;    
-    float hcannyf;
+        //ACCV
+        double fx = 572.41140;
+        double fy = 573.57043;
+        double cx = 325.26110;
+        double cy = 242.04899;    
+        
+        float k_vec[9];
+        float kfloat;
+        float kxfloat;
+        float kyfloat;
+        float ksfloat;
+        float gafloat;
+        float lafloat;
+        float lcannyf;    
+        float hcannyf;
 };
 
 Facade::Facade(std::string configFilePath)
@@ -98,45 +107,40 @@ Facade::Facade(std::string configFilePath)
     gafloat = ((float)g_angle)*deg2rad;
     lafloat = ((float)l_angle)*deg2rad;
     lcannyf = (float)Lcanny/1000.f;
-    hcannyf = (float)Hcanny/1000.f;    
+    hcannyf = (float)Hcanny/1000.f;   
 }
 
-ObjectSeg* Facade::segmentImage(const char* rgbFilePath, const char* depthFilePath, char *numObjects) {
-    cv::Mat kinect_rgb_img = cv::imread(rgbFilePath);//,cv::IMREAD_UNCHANGED);
-    cv::Mat kinect_depth_img_mm = cv::imread(depthFilePath,cv::IMREAD_UNCHANGED);// in mm
-
+boost::python::list Facade::segmentImage(cv::Mat kinect_rgb_img, cv::Mat kinect_depth_img_mm) {
     GraphCanny::GraphCannySeg<GraphCanny::hsv>* gcs = new GraphCanny::GraphCannySeg<GraphCanny::hsv>(kinect_rgb_img, kinect_depth_img_mm, sigma, kfloat, min_size, kxfloat, kyfloat, ksfloat,k_vec,lcannyf,hcannyf,kdv, kdc,max_ecc,max_L1,max_L2,(uint16_t)DTH,(uint16_t)plusD,(uint16_t)point3D,gafloat,lafloat,(float)FarObjZ);    
     gcs->showImages = showImages;    
     gcs->showDebug = showDebug;
     gcs->run();
 
     std::vector<GraphCanny::SegResults> vecSegResult = gcs->vecSegResults;
-    int maxLength = vecSegResult.size();
-    strcpy(numObjects, std::to_string(maxLength).c_str());
+    this->numObjects = vecSegResult.size();
 
-    ObjectSeg* results = new ObjectSeg[maxLength];
+    boost::python::list results;
 
-    for(std::size_t objs=0; objs < maxLength; ++objs) {
+    for(std::size_t objs=0; objs < this->numObjects; ++objs) {
         GraphCanny::SegResults obj = vecSegResult[objs];
         int numPoints = obj.pxs.size();
 
-        results[objs] = ObjectSeg(objs+1, numPoints, new PointSeg[numPoints]);
+        boost::python::list points;
+        results.append(ObjectSeg(objs+1, numPoints, points));
 
         for(std::size_t pixel=0; pixel < numPoints; ++pixel) {
             cv::Point3i point = obj.pxs[pixel]; 
-            results[objs].points[pixel].x = point.x;
-            results[objs].points[pixel].y = point.y;
-            results[objs].points[pixel].z = point.z;
+            points.append(PointSeg(point.x,point.y,point.z));
         }        
     }
 
     if (showDebug)
-        printf("Resulting a array of %d\n", maxLength);
+        printf("Resulting a array of %d\n", this->numObjects);
 
     return results;
 }
 
-void Facade::cleanupObjects(ObjectSeg* objectToClean, char *numObjects) {
+/*void Facade::cleanupObjects(ObjectSeg* objectToClean, char *numObjects) {
     if (objectToClean) {
         int numObjts = atoi(numObjects);
 
@@ -148,16 +152,62 @@ void Facade::cleanupObjects(ObjectSeg* objectToClean, char *numObjects) {
 
         delete[] objectToClean;
     }      
-} 
+}  */
 
-extern "C"
-{   
-    Facade* Facade_new(const char* configFilePath) {return new Facade(std::string(configFilePath));}
-    ObjectSeg* Facade_segmentImage(Facade* facade, const char* rgbFilePath, const char* depthFilePath, char *numObjects) {
-        return facade->segmentImage(rgbFilePath, depthFilePath, numObjects);
+Facade *Facade_new(const char* configFilePath) {
+    return new Facade(std::string(configFilePath));
+}
+
+boost::python::list Facade_segmentImage(Facade *facade, PyObject *rgbImage, PyObject *depthImage) {
+    cv::Mat kinect_rgb_img = pbcvt::fromNDArrayToMat(rgbImage);
+    cv::Mat kinect_depth_img_mm = pbcvt::fromNDArrayToMat(depthImage);
+
+    return facade->segmentImage(kinect_rgb_img, kinect_depth_img_mm);
+}
+
+/*void Facade_cleanupObjects(Facade *facade, boost::python::list objectToClean) {               
+    facade->cleanupObjects(objectToClean);
+}*/
+
+#if (PY_VERSION_HEX >= 0x03000000)
+
+    static void *init_ar() {
+#else
+        static void init_ar(){
+#endif
+        Py_Initialize();
+
+        import_array();
+        return NUMPY_IMPORT_ARRAY_RETVAL;
     }
 
-    void Facade_cleanupObjects(Facade* facade, ObjectSeg* objectToClean, char *numObjects) {
-        facade->cleanupObjects(objectToClean, numObjects);
-    }      
-}
+    BOOST_PYTHON_MODULE (libgraph_canny_segm) {
+        //using namespace XM;
+        init_ar();
+
+        //initialize converters
+        to_python_converter<cv::Mat,pbcvt::matToNDArrayBoostConverter>();
+        matFromNDArrayBoostConverter();
+
+        //expose module-level functions
+        class_<Facade, boost::shared_ptr<Facade>>("Facade", init<const char*>())
+            .def_readwrite("numObjects", &Facade::numObjects);   
+
+        class_<PointSeg>("PointSeg")
+            .def_readwrite("x", &PointSeg::x)
+            .def_readwrite("y", &PointSeg::y)
+            .def_readwrite("z", &PointSeg::z);
+
+        class_<ObjectSeg>("ObjectSeg")
+            .def_readwrite("id", &ObjectSeg::id)
+            .def_readwrite("pointsLength", &ObjectSeg::pointsLength)            
+            .def_readwrite("points", &ObjectSeg::points);
+        class_<boost::python::list>("ObjectSeg_vector");                 
+  
+        //expose module-level functions
+        def("Facade_new", &Facade_new, return_value_policy<manage_new_object>());
+        def("Facade_segmentImage", &Facade_segmentImage);
+        //def("Facade_cleanupObjects", Facade_cleanupObjects);
+    }
+
+} //end namespace pbcvt
